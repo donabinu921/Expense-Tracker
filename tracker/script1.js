974574a9ba75b555c23a26755a658c3b72f3893f
donabinu921 <elizabethdona921@gmail.com>
2023-05-24 18:53:07 +0530

    Added delete functionality

--- tracker/script1.js
+++ tracker/script1.js
@@ -12,10 +12,21 @@ document
     var descriptionCell = row.insertCell(0);
     var amountCell = row.insertCell(1);
     var dateCell = row.insertCell(2);
+    var deleteCell = row.insertCell(3);
 
     descriptionCell.textContent = description;
     amountCell.textContent = "Rs " + amount.toFixed(2);
     dateCell.textContent = date.toUTCString().slice(5, 16);
+
+    var deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.classList.add("delete-button");
+    deleteButton.addEventListener("click", function () {
+      var row = this.parentNode.parentNode;
+      table.deleteRow(row.rowIndex);
+    });
+
+    deleteCell.appendChild(deleteButton);
 
     document.getElementById("expenseDescription").value = "";
     document.getElementById("expenseAmount").value = "";
