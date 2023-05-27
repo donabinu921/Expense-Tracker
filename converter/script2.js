455d2298f86089174db8bf5c944f43ced672e655
donabinu921 <elizabethdona921@gmail.com>
2023-05-27 13:18:32 +0530

    Changed the api; set currency input to dropdowns

--- converter/script2.js
+++ converter/script2.js
@@ -1,41 +1,57 @@
+const baseUrl = "https://v6.exchangerate-api.com/v6/";
+const apiKey = "6059e2e91b1d55171f4e1d65";
 const inputFields = document.querySelectorAll(".inputField");
+const form = document.getElementById("currConverter");
+const firstCurrencyInput = document.getElementById("firstCurrency");
+const secondCurrencyInput = document.getElementById("secondCurrency");
+const amountInput = document.getElementById("amount");
+const convertedAmountElement = document.getElementById("convertedAmount");
 
-inputFields.forEach(function(inputField) {
-  inputField.addEventListener("input", function(event) {
-    const userInput = event.target.value;
-    event.target.value = userInput.toUpperCase();
+async function getCurrencyList() {
+  const response = await fetch(`${baseUrl}${apiKey}/codes`);
+  const data = await response.json();
+  const currency = data.supported_codes;
+  const firstElements = currency.map(([firstElement]) => firstElement);
+  return firstElements;
+}
+
+async function populateCurrencyOptions() {
+  const currencyList = await getCurrencyList();
+  currencyList.forEach((currency) => {
+    const option1 = document.createElement("option");
+    option1.value = currency;
+    option1.textContent = currency;
+    firstCurrencyInput.appendChild(option1);
+
+    const option2 = document.createElement("option");
+    option2.value = currency;
+    option2.textContent = currency;
+    secondCurrencyInput.appendChild(option2);
   });
-});
+}
 
-
-const form = document.getElementById('currConverter');
-const firstCurrencyInput = document.getElementById('firstCurrency');
-const secondCurrencyInput = document.getElementById('secondCurrency');
-const amountInput = document.getElementById('amount');
-const convertedAmountElement = document.getElementById('convertedAmount');
+populateCurrencyOptions();
 
 function convertCurrency(event) {
   event.preventDefault();
 
-  const firstCurrency = firstCurrencyInput.value.toUpperCase();
-  const secondCurrency = secondCurrencyInput.value.toUpperCase();
+  const firstCurrency = firstCurrencyInput.value;
+  const secondCurrency = secondCurrencyInput.value;
   const amount = parseFloat(amountInput.value);
 
   convert(firstCurrency, secondCurrency, amount);
 }
 
-const baseUrl="https://www.amdoren.com/api/currency.php";
-const apiKey="feKEHL8W35yR3MfNQM85fm7GR8k29A";
-
-async function convert(first,second,amnt){
-    const response=await fetch(`${baseUrl}?api_key=${apiKey}&from=${first}&to=${second}&amount=${amnt}`);
-    const data=await response.json();
-    let convertedAmount= data.amount;
-    convertedAmountElement.textContent = `${amnt} ${first} = ${convertedAmount} ${second}`;
-    document.getElementById("firstCurrency").value = "";
-    document.getElementById("secondCurrency").value = "";
-    document.getElementById("amount").value = "";
-
+async function convert(first, second, amnt) {
+  const response = await fetch(
+    `${baseUrl}${apiKey}/pair/${first}/${second}/${amnt}`
+  );
+  const data = await response.json();
+  let convertedAmount = data.conversion_result;
+  convertedAmountElement.textContent = `${amnt} ${first} = ${convertedAmount} ${second}`;
+  firstCurrencyInput.value = "";
+  secondCurrencyInput.value = "";
+  amountInput.value = "";
 }
 
-form.addEventListener('submit', convertCurrency);
+form.addEventListener("submit", convertCurrency);
